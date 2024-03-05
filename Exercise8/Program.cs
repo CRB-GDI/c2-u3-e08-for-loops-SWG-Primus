b69ebae667df38b11baf4095195c0683ec4157a0
SWG-Primus <Primus.Odom@s.capregboces.org>
2024-03-05 09:31:01 -0500

    Completed Exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter maximum number: ");
+            int maxNum = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < maxNum; i++)
+            {
+                //for loop body
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            //print asterisk row
+            Console.WriteLine("How many rows of Asterisks?: ");
+            int astNum = Int32.Parse(Console.ReadLine());
+
+            for (int a = 0; a < astNum; a++)
+            {
+                Console.Write("*");
+            }
+
+            //Console.WriteLine(number);
+        }
+    }
+}
